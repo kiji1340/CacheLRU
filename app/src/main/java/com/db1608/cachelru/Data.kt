813a6991380b2e401d1676dc0fdf484b5dc250fa
Nguyen Ngoc Dinh Bao <dinhbao.nguyen@nhnent.com>
2019-07-10 17:05:31 +0700

    update code

---
+++ app/src/main/java/com/db1608/cachelru/Data.kt
@@ -0,0 +1,4 @@
+package com.db1608.cachelru
+
+data class Data(var name: String?, var content: String?)
+
--- app/src/main/java/com/db1608/cachelru/MainActivity.kt
+++ app/src/main/java/com/db1608/cachelru/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.db1608.cachelru
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+import com.db1608.cache.CacheLRU
+import com.db1608.cache.CacheLRUBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    private var encrypted = false
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        encrypted = isChecked
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            initializeBtn -> {
+
+                CacheLRUBuilder.configure(8152)
+                    .initialize()
+            }
+
+            saveBtn -> {
+                val data = Data(nameEd.text.toString(), contentEd.text.toString())
+                CacheLRU.put("data", data)
+            }
+
+            clearBtn -> {
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        encryptCB.setOnCheckedChangeListener(this)
+        saveBtn.setOnClickListener(this)
+        clearBtn.setOnClickListener(this)
     }
 }
